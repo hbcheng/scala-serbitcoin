358693ba8bf23dcc779526994265763a7b6a5e4f
Hao Bryan Cheng <haobcheng@gmail.com>
2017-11-02 13:56:32 -0700

    Finish implementation of addressHash, fix addressBytes

--- src/main/scala/xyz/bcheng/bitcoinser/script/Analyze.scala
+++ src/main/scala/xyz/bcheng/bitcoinser/script/Analyze.scala
@@ -1,8 +1,8 @@
 package xyz.bcheng.bitcoinser.script
 
-import org.bitcoinj.core.{Base58, Sha256Hash}
+import org.bitcoinj.core.{Base58, Sha256Hash, Utils}
 
-class Analyze(addrPrefix: Byte, p2shAddrPrefix: Byte, msingAddrPrefix: Byte) {
+class Analyzer(addrPrefix: Byte, p2shAddrPrefix: Byte) {
   def extractScriptType(scriptPubKey: Option[List[Byte]], scriptSig: Option[List[Byte]]): ScriptType = {
     new Unknown(scriptPubKey, scriptSig)  
   }
@@ -15,7 +15,7 @@ class Analyze(addrPrefix: Byte, p2shAddrPrefix: Byte, msingAddrPrefix: Byte) {
           case st => extractAddress(st)
         }
       }
-      case P2SH(sh) => extractP2SHAddress(sh)
+      case P2SH(sh) => scriptHashToAddress(sh)
       case P2PKH(pkh) => pubkeyHashToAddress(pkh)
       case P2PK(pk) => pubkeyToAddress(pk)
       case P2WPKH(_) | P2WSH(_) => {
@@ -29,13 +29,11 @@ class Analyze(addrPrefix: Byte, p2shAddrPrefix: Byte, msingAddrPrefix: Byte) {
  
   // A P2SH address is the scriptHash, plus the bit indicating the type,
   // and a checksum
-  def extractP2SHAddress(sh: List[Byte]): Option[String] = {
-    // Not yet implemented
+  def scriptHashToAddress(sh: List[Byte]): Option[String] = {
     Some(Base58.encode((addressBytes(sh, p2shAddrPrefix).toArray)))
   }
 
   def pubkeyHashToAddress(pkh: List[Byte]): Option[String] = {
-    // Not yet implemented
     Some(Base58.encode(addressBytes(pkh, addrPrefix).toArray))
   }
 
@@ -46,10 +44,11 @@ class Analyze(addrPrefix: Byte, p2shAddrPrefix: Byte, msingAddrPrefix: Byte) {
   // Pubkey to pubkey hash
   def addressHash(pk: List[Byte]): List[Byte] = {
     // Not yet implemented
-    pk
+    Utils.sha256hash160(pk.toArray).toList
   }
 
   def addressBytes(payload: List[Byte], prefix: Byte): List[Byte] = {
-    prefix :: payload ::: Sha256Hash.twiceOf(payload.toArray).getBytes().toList
+    val checksum = Sha256Hash.twiceOf((prefix :: payload).toArray).getBytes().toList
+    prefix :: payload ::: checksum.slice(0, 4)
   }
 }
