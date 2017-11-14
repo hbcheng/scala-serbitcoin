c9dde340d1e84bafe7571ef171f1017d35a79fe9
Hao Bryan Cheng <haobcheng@gmail.com>
2017-11-14 12:27:52 -0800

    Extend Analyzer with script type extractions for scriptPubKeys

--- src/main/scala/xyz/bcheng/bitcoinser/script/Analyze.scala
+++ src/main/scala/xyz/bcheng/bitcoinser/script/Analyze.scala
@@ -2,16 +2,70 @@ package xyz.bcheng.bitcoinser.script
 
 import org.bitcoinj.core.{Base58, Sha256Hash, Utils}
 
-class Analyzer(addrPrefix: Byte, p2shAddrPrefix: Byte) {
-  def extractScriptType(scriptPubKey: Option[List[Byte]], scriptSig: Option[List[Byte]]): ScriptType = {
-    new Unknown(scriptPubKey, scriptSig)  
+object Analyzer {
+  // Having None for script pub key or script sig means that the relevant script
+  // is not known, _NOT_ that it is empty. An empty script should have an empty
+  // list.
+  def extractScriptType(scriptPubKey: Option[List[Byte]], scriptSig: Option[List[Byte]], witness: Option[List[List[Byte]]]): ScriptType = {
+    (guessScriptPubKeyType(scriptPubKey) match {
+      case Some(st) => st
+      case None => Unknown(scriptPubKey, None, None)
+    }).refineWith(scriptPubKey, scriptSig, witness)
   }
 
+  def guessScriptPubKeyType(scriptPubKey: Option[List[Byte]]): Option[ScriptType] = {
+    if (scriptPubKey.isEmpty) {
+      None
+    } else {
+      val spk = scriptPubKey.get
+      spk.length match {
+        case 25 => tryPayToPubkeyHash(spk)
+        case 23 => tryPayToScriptHash(spk)
+        case 35 | 67 => tryPayToPubkey(spk)
+        case _ => None //checkRawMultisig(spk)
+      }
+    }
+  }
+
+  // Expects length to be verified to be 25.
+  private def tryPayToPubkeyHash(scriptPubKey: List[Byte]) : Option[ScriptType] = {
+    println("Entered tryPayToPubkeyHash")
+    println(scriptPubKey)
+    if (scriptPubKey(0) == OpCodes.OP_DUP && scriptPubKey(1) == OpCodes.OP_HASH160 && scriptPubKey(2) == 0x14.toByte && scriptPubKey(23) == OpCodes.OP_EQUALVERIFY && scriptPubKey(24) == OpCodes.OP_CHECKSIG) {
+      println("matched")
+      Some(new P2PKH(scriptPubKey.slice(3,23)))
+    } else {
+      println("not matched")
+      None
+    }
+  }
+
+  // Expects length verified to 23
+  private def tryPayToScriptHash(scriptPubKey: List[Byte]) : Option[ScriptType] = {
+    if (scriptPubKey(0) == OpCodes.OP_HASH160 && scriptPubKey(1) == 0x14.toByte && scriptPubKey(22) == OpCodes.OP_EQUAL) {
+      Some(new P2SH(scriptPubKey.slice(2,22)))
+    } else {
+      None
+    }
+  }
+
+  private def tryPayToPubkey(scriptPubKey: List[Byte]) : Option[ScriptType] = {
+    if (scriptPubKey(scriptPubKey.length - 1) == OpCodes.OP_CHECKSIG) {
+      Some(new P2PK(scriptPubKey.slice(1, scriptPubKey.length-1)))
+    } else {
+      None
+    }
+  }
+
+
+}
+
+class Analyzer(addrPrefix: Byte, p2shAddrPrefix: Byte) {
   def extractAddress(stype: ScriptType): Option[String] = {
     stype match {
-      case Unknown(spk, ss) => {
-        extractScriptType(spk, ss) match {
-          case Unknown(_, _) => None
+      case Unknown(spk, ss, wit) => {
+        Analyzer.extractScriptType(spk, ss, wit) match {
+          case Unknown(_, _, _) => None
           case st => extractAddress(st)
         }
       }
